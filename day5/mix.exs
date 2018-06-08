9a34b3c2e456a73ecce0496ee8a91ee243306c6d
Scott Keller <scott.keller13@gmail.com>
2018-06-08 16:29:38 -0400

    Add mix test.watch

--- day5/mix.exs
+++ day5/mix.exs
@@ -15,6 +15,6 @@ defmodule Day5.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [{:mix_test_watch, "~> 0.6", only: :dev, runtime: false}]
   end
 end

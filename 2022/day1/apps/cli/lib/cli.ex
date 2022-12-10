023945bce5ed4bde70aa1f6297709530b580808e
Scott Keller <scott.keller13@gmail.com>
2022-12-10 12:41:54 -0500

    Refactor Cli code for readability and flexibility

--- 2022/day1/apps/cli/lib/cli.ex
+++ 2022/day1/apps/cli/lib/cli.ex
@@ -5,20 +5,34 @@ defmodule Cli do
 
   @doc false
   def main(args) do
+    with {:ok, filename} <- parse_args(args),
+         :ok <- file_exists?(filename),
+         {:ok, output} <-
+           CalorieCounter.process(filename) do
+      IO.puts(output)
+    else
+      {:error, msg} ->
+        IO.puts(msg)
+        System.stop(1)
+    end
+  end
+
+  @doc false
+  defp parse_args(args) do
     case args do
       [] ->
-        IO.puts("Please provide a file name")
-        System.halt(1)
+        {:error, "Please provide a file name"}
 
       [filename | _] ->
-        case CalorieCounter.process(filename) do
-          {:ok, output} ->
-            IO.puts(output)
+        {:ok, filename}
+    end
+  end
 
-          {:error, msg} ->
-            IO.puts(msg)
-            System.stop(1)
-        end
+  @doc false
+  defp file_exists?(filename) do
+    case File.exists?(filename) do
+      true -> :ok
+      false -> {:error, "File #{filename} does not exist"}
     end
   end
 end

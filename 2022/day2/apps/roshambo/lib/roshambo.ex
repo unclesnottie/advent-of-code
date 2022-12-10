4c77229142668f1d7311cc504c1ce86cc3556020
Scott Keller <scott.keller13@gmail.com>
2022-12-10 16:33:17 -0500

    Implement solution to part 2

--- 2022/day2/apps/roshambo/lib/roshambo.ex
+++ 2022/day2/apps/roshambo/lib/roshambo.ex
@@ -13,6 +13,16 @@ defmodule Roshambo do
     "Z" => :scissors
   }
 
+  # Maps input characters to :rock, :paper and :scissors or :win, :lose, :draw
+  @input_converter2 %{
+    "A" => :rock,
+    "B" => :paper,
+    "C" => :scissors,
+    "X" => :lose,
+    "Y" => :draw,
+    "Z" => :win
+  }
+
   # Maps :rock, :paper and :scissors to point value
   @shape_points %{
     rock: 1,
@@ -31,7 +41,7 @@ defmodule Roshambo do
   The main entry point into Roshambo
   """
   def process(filename) when is_binary(filename) do
-    part_one_impl(filename)
+    part_two_impl(filename)
   end
 
   # Solution for part one
@@ -39,6 +49,16 @@ defmodule Roshambo do
     total =
       File.stream!(filename)
       |> Stream.map(&input_to_round_score/1)
+      |> Enum.sum()
+
+    {:ok, total}
+  end
+
+  # Solution for part two
+  def part_two_impl(filename) when is_binary(filename) do
+    total =
+      File.stream!(filename)
+      |> Stream.map(&input_to_round_score2/1)
       |> Enum.sum()
 
     {:ok, total}
@@ -56,12 +76,40 @@ defmodule Roshambo do
     calc_score(them, you)
   end
 
+  # Converts round input to score part 2
+  defp input_to_round_score2(input) when is_binary(input) do
+    list =
+      input
+      |> String.graphemes()
+
+    them = list |> Enum.at(0)
+    outcome = list |> Enum.at(2)
+
+    calc_score2(them, outcome)
+  end
+
   # Calculates the score from a round of roshambo
   defp calc_score(them, you) do
     their_shape = @input_converter[them]
     your_shape = @input_converter[you]
     @shape_points[your_shape] + @outcome_points[roshambo(their_shape, your_shape)]
   end
+
+  # Calculates the score from a round of roshambo
+  defp calc_score2(them, outcome) do
+    their_shape = @input_converter2[them]
+    the_outcome = @input_converter2[outcome]
+    @shape_points[get_shape(their_shape, the_outcome)] + @outcome_points[the_outcome]
+  end
+
+  # Gets needed shape to achieve outcome
+  defp get_shape(shape, :draw), do: shape
+  defp get_shape(:rock, :win), do: :paper
+  defp get_shape(:rock, :lose), do: :scissors
+  defp get_shape(:paper, :win), do: :scissors
+  defp get_shape(:paper, :lose), do: :rock
+  defp get_shape(:scissors, :win), do: :rock
+  defp get_shape(:scissors, :lose), do: :paper
 
   # Calcluates outcome of a roshambo round
   defp roshambo(shape, shape), do: :draw

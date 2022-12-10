5d3809d31574211137a2d29b3de3bdee06c35415
Scott Keller <scott.keller13@gmail.com>
2022-12-10 16:11:11 -0500

    Implement solution for part 1

--- 2022/day2/apps/cli/lib/cli.ex
+++ 2022/day2/apps/cli/lib/cli.ex
@@ -4,15 +4,36 @@ defmodule CLI do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CLI.hello()
-      :world
-
+  The main function for the command line interface.
   """
-  def hello do
-    :world
+  def main(args) do
+    with {:ok, filename} <- parse_args(args),
+         :ok <- file_exists?(filename),
+         {:ok, output} <- Roshambo.process(filename) do
+      IO.puts(output)
+    else
+      {:error, msg} ->
+        IO.puts(msg)
+        System.stop(1)
+    end
+  end
+
+  # Parses command line arguments to get the input file name
+  defp parse_args(args) do
+    case args do
+      [] ->
+        {:error, "Please provide a file name"}
+
+      [filename | _] ->
+        {:ok, filename}
+    end
+  end
+
+  # Verifies the input file exists
+  defp file_exists?(filename) when is_binary(filename) do
+    case File.exists?(filename) do
+      true -> :ok
+      false -> {:error, "File #{filename} does not exist"}
+    end
   end
 end
--- 2022/day2/apps/roshambo/lib/roshambo.ex
+++ 2022/day2/apps/roshambo/lib/roshambo.ex
@@ -3,16 +3,67 @@ defmodule Roshambo do
   Documentation for `Roshambo`.
   """
 
+  # Maps input characters to :rock, :paper and :scissors
+  @input_converter %{
+    "A" => :rock,
+    "B" => :paper,
+    "C" => :scissors,
+    "X" => :rock,
+    "Y" => :paper,
+    "Z" => :scissors
+  }
+
+  # Maps :rock, :paper and :scissors to point value
+  @shape_points %{
+    rock: 1,
+    paper: 2,
+    scissors: 3
+  }
+
+  # Maps :win, :lose and :draw to point value
+  @outcome_points %{
+    win: 6,
+    lose: 0,
+    draw: 3
+  }
+
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Roshambo.hello()
-      :world
-
+  The main entry point into Roshambo
   """
-  def hello do
-    :world
+  def process(filename) when is_binary(filename) do
+    total =
+      File.stream!(filename)
+      |> Stream.map(&input_to_round_score/1)
+      |> Enum.sum()
+
+    {:ok, total}
   end
+
+  # Converts round input to score
+  defp input_to_round_score(input) when is_binary(input) do
+    list =
+      input
+      |> String.graphemes()
+
+    them = list |> Enum.at(0)
+    you = list |> Enum.at(2)
+
+    calc_score(them, you)
+  end
+
+  # Calculates the score from a round of roshambo
+  defp calc_score(them, you) do
+    their_shape = @input_converter[them]
+    your_shape = @input_converter[you]
+    @shape_points[your_shape] + @outcome_points[roshambo(their_shape, your_shape)]
+  end
+
+  # Calcluates outcome of a roshambo round
+  defp roshambo(shape, shape), do: :draw
+  defp roshambo(:rock, :paper), do: :win
+  defp roshambo(:rock, :scissors), do: :lose
+  defp roshambo(:paper, :scissors), do: :win
+  defp roshambo(:paper, :rock), do: :lose
+  defp roshambo(:scissors, :rock), do: :win
+  defp roshambo(:scissors, :paper), do: :lose
 end
